407e57491d7b290eb3cae08697cecaf4d01fdff7
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-15 19:52:54 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,24 @@
 class Post 
+  attr_accessor :author, :name
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil 
+    end
+  end
   
   
 end
