01c8b52fb38b4bbd1dec44d8dd1d15f69d0ce8b1
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-15 19:44:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,7 @@ class Artist
   
   def add_song(song)
     song.artist = self
-    
+    @@song_count +=1
   end
  
   def songs
@@ -20,6 +20,7 @@ class Artist
    def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
+    @@song_count +=1
   end
   
   def self.song_count
